af7f03f5ede20aa5b8e362a531b828880d0ff3a3
Jonas Nunes <jonas.nunees@yahoo.com.br>
2022-03-28 20:21:54 -0300

    update index and style

--- mini-projetos/lista-de-carros/index.php
+++ mini-projetos/lista-de-carros/index.php
@@ -4,11 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Carros</title>
+    <title>Catálogo de Carros</title>
 
     <link rel="stylesheet" href="./_estilos/index.css">
 </head>
 <body>
-    
+    <div id="corpo">
+        <h1>Catálogo de Carros</h1>
+        <table class="listagem">
+            <tr>
+                <td>Foto</td>
+                <td>Nome</td>
+                <td>Adm</td>
+            </tr>
+            <tr>
+                <td>Foto</td>
+                <td>Nome</td>
+                <td>Adm</td>
+            </tr>
+            <tr>
+                <td>Foto</td>
+                <td>Nome</td>
+                <td>Adm</td>
+            </tr>
+            <tr>
+                <td>Foto</td>
+                <td>Nome</td>
+                <td>Adm</td>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
