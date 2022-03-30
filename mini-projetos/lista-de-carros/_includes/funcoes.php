424990d8b4a1242683fd4bd7e4b953c0b4035e9f
Jonas Nunes <jonas.nunees@yahoo.com.br>
2022-03-29 23:21:39 -0300

    update project

---
+++ mini-projetos/lista-de-carros/_includes/funcoes.php
@@ -0,0 +1,13 @@
+<?php
+
+    function thumb($arquivo){
+        $caminho = "fotos/$arquivo";
+        if(is_null($arquivo) || !file_exists($caminho)){
+            return "fotos/indisponivel.png";
+        }
+        else{
+            return $caminho;
+        }
+    }
+
+?>
--- mini-projetos/lista-de-carros/index.php
+++ mini-projetos/lista-de-carros/index.php
@@ -9,30 +9,40 @@
     <link rel="stylesheet" href="./_estilos/index.css">
 </head>
 <body>
+
+    <?php
+        require_once "includes/banco.php";
+        require_once "includes/funcoes.php";
+    ?>
+
     <div id="corpo">
         <h1>Catálogo de Carros</h1>
         <table class="listagem">
-            <tr>
-                <td>Foto</td>
-                <td>Nome</td>
-                <td>Adm</td>
-            </tr>
-            <tr>
-                <td>Foto</td>
-                <td>Nome</td>
-                <td>Adm</td>
-            </tr>
-            <tr>
-                <td>Foto</td>
-                <td>Nome</td>
-                <td>Adm</td>
-            </tr>
-            <tr>
-                <td>Foto</td>
-                <td>Nome</td>
-                <td>Adm</td>
-            </tr>
+
+            <?php
+                $busca = $banco->query("select * from bd_carros order by nome");
+                if(!$busca){
+                    echo "<tr><td>Erro na busca! Tente novamente...</td></tr>";
+                }
+                else{
+                    if(!$busca->num_rows == 0){
+                        echo "<tr><td>Nenhum registro encontrado!</td></tr>";
+                    }
+                    else{
+                        while($registro = $busca->fetch_object()){
+                            $t = thumb($registro->logo);
+                            echo "<tr><td><img src='$t' class='mini'/><td>$registro->modelo";
+                            echo "<td>Adm";
+                        }
+                    }
+                }
+            ?>
+
         </table>
     </div>
+
+    <?php
+        $banco->close();
+    ?>
 </body>
 </html>
