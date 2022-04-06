32ab120da36552ff083f57cdaba15c2bad17c4fd
Jonas Nunes <jonas.nunees@yahoo.com.br>
2022-04-05 21:09:25 -0300

    update project

--- mini-projetos/lista-de-carros-v2/_scripts/index.js
+++ mini-projetos/lista-de-carros-v2/_scripts/index.js
@@ -5,20 +5,3 @@ const typed = new Typed('.name', {
     backDelay: 500,
     loop: true
   });
-
-const imgs = document.getElementById("img");
-const img = document.querySelectorAll("#img img");
-
-let idx = 0;
-
-function carrossel(){
-    idx++;
-
-    if(idx > img.length - 1){
-        idx = 0;
-    }
-
-    imgs.style.transform = `translateX(${-idx * 500}px)`;
-}
-
-setInterval(carrossel, 1800);
--- mini-projetos/lista-de-carros-v2/index.php
+++ mini-projetos/lista-de-carros-v2/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catálogo</title>
 
-    <link rel="stylesheet" href="_estilos/estilo.css">
+    <link rel="stylesheet" href="_estilos/index.css">
 </head>
 <body>
     <header>
@@ -19,13 +19,11 @@
     </header>
     <main>
         <p>Aqui você encontra - <span class="name"></span></p>
-        <section class="carrossel">
-            <div class="container" id="img">
-                <img src="_imagens/logo-chevrolet.png" alt="logo chevrolet">
-                <img src="_imagens/logo-fiat.png" alt="logo fiat">
-                <img src="_imagens/logo-ford.png" alt="logo ford">
-                <img src="_imagens/logo-volks.png" alt="logo volkswagen">
-            </div>
+        <section class="imagens">
+            <img src="_imagens/logo-chevrolet.png" alt="logo chevrolet">
+            <img src="_imagens/logo-fiat.png" alt="logo fiat">
+            <img src="_imagens/logo-ford.png" alt="logo ford">
+            <img src="_imagens/logo-volks.png" alt="logo volkswagen">
         </section>
     </main>
     <footer>
